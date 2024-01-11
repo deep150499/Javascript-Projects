a836a53fba99cb1c7aaa00151c7ce4f525febe9f
Deep Nanavati <119634445+deep150499@users.noreply.github.com>
2024-01-11 18:00:15 -0500

    Added files

---
+++ Javascript-Project1/script.js
@@ -0,0 +1,12 @@
+let index = 0;
+
+function changeColors() {
+
+    var colors = ['red','blue','orange','purple','green','yellow']
+
+    document.getElementsByTagName('body')[0].style.background = colors[index++];
+
+    if(index > colors.length -1){
+        index = 0;
+    }
+}
