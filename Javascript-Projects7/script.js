f3fbdb52a2863253c32869fa5ff5b82fb3f7a6d4
Deep Nanavati <119634445+deep150499@users.noreply.github.com>
2024-01-16 19:32:05 -0500

    Commited files

---
+++ Javascript-Projects7/script.js
@@ -0,0 +1,26 @@
+const passwordBox = document.getElementById('password');
+const length = 12;
+
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '1234567890';
+const specialCharaters = '!@#$%^&*()';
+const allChars = upperCase + lowerCase + numbers + specialCharaters;
+
+function generatePassword() {
+    let password = '';
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += specialCharaters[Math.floor(Math.random() * specialCharaters.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand('copy');
+}
